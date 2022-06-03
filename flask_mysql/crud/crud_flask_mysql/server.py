901b775bc70148503b28d7b13b4a682f43442dab
Jesse Dods <ghostling425@gmail.com>
2022-06-02 18:09:43 -0700

    updated to add: showUser, editUser and DeleteUser

--- flask_mysql/crud/crud_flask_mysql/server.py
+++ flask_mysql/crud/crud_flask_mysql/server.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, render_template, request, redirect
 from user import User
 
@@ -8,19 +9,19 @@ app.secret_key = 'keep me secret, keep me safe'
 def show_data():
     users = User.get_rows() #creates a value, get to the return from the get_all def in User.py
     print(users) #prints the returned information inside the users value
-    return render_template("read.html", all_users = users) #pops the index.html page and 
+    return render_template("read.html", all_users = users)
 
 @app.route("/create")
 def create():
     return render_template("create.html") #pops the index.html page
 
-@app.route("/show")
-def show():
-    return render_template("show.html") #pops the index.html page
+# @app.route("/show")
+# def show():
+#     return render_template("show.html") #pops the index.html page
 
-@app.route("/edit")
-def edit():
-    return render_template("edit.html") #pops the index.html page
+# @app.route("/edit")
+# def edit():
+#     return render_template("edit.html") #pops the index.html page
 
 
 
@@ -36,15 +37,35 @@ def add_user():
         "last_name" : request.form["last_name"],
         "email" : request.form["email"]
     }
-# this guy goe to our class User, to our save def, which adds(saves) our entered data from the form
+# takes the information above and sends it the database. then redirects
     User.save(data)
     return redirect("/")
 
+# select one user
+@app.route("/select_one/<int:id>")
+def select(id):
+    data= {"id": id}
+    return render_template ("show.html", user = User.select_one(data))
+
+# Need to edit(UPDATE)
+# grabbing the 
+@app.route("/edit_one/<int:id>")
+def edit(id):
+    data ={"id":id}
+    return render_template("edit.html", user=User.select_one(data))
+
+@app.route("/update_edit", methods=["POST"])
+def update():
+    User.save_edit(request.form)
+    return redirect("/")
 
 
-
-
-
+# Need to delete(DELETE)
+@app.route("/delete_user/<int:id>")
+def delete(id):
+    data ={"id":id}
+    User.delete_user(data)
+    return redirect("/")
 
 
 
--- flask_mysql/crud/crud_flask_mysql/user.py
+++ flask_mysql/crud/crud_flask_mysql/user.py
@@ -19,7 +19,7 @@ class User:
 
         users = []
         for user in results:
-            users.append( User(user) )
+            users.append( cls(user) )
         return users
 
 # this class method allows us to send info to our DB user_entry
@@ -27,3 +27,26 @@ class User:
     def save(cls, data):
         query = "INSERT INTO users (first_name, last_name, email, create_at, update_at) VALUES (%(first_name)s, %(last_name)s, %(email)s, NOW(), NOW() );"
         return connectToMySQL("user_entry").query_db( query, data )
+
+# class that SELECTS ONE user
+    @classmethod
+    def select_one(cls, data):
+        query = "SELECT * FROM users WHERE id = %(id)s;"
+        print(query)
+        results = connectToMySQL("user_entry").query_db(query, data)
+        print(results)
+        return cls(results[0])
+
+# edit(UPDATE) classmethod for the ONE user
+    @classmethod
+    def save_edit(cls, data):
+        query = "UPDATE users SET first_name = %(first_name)s, last_name = %(last_name)s, email = %(email)s, update_at = NOW() WHERE id = %(id)s;"
+        print(query)
+        return connectToMySQL("user_entry").query_db(query, data)
+
+# delete classmethod for a selected user
+    @classmethod
+    def delete_user(cls,data):
+        query = "DELETE FROM users WHERE id = %(id)s;"
+        print(query)
+        return connectToMySQL("user_entry").query_db(query, data)
